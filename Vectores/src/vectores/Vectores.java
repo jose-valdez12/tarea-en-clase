4f45b64636d36ed5db75c56c0b7ff4abd645b188
jose-valdez12 <90970578+jose-valdez12@users.noreply.github.com>
2022-05-14 13:46:11 -0600

    tarea

---
+++ Vectores/src/vectores/Vectores.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package vectores;
+
+
+
+/**
+ *
+ * @author jvald
+ */
+public class Vectores {
+
+    /**
+     * @param args the command line arguments
+     */
+    @SuppressWarnings("empty-statement")
+    public static void main(String[] args) {
+    
+        int num;
+        System.out.println("Secuencia de numeros");
+        System.out.println("Del 0 al 100");
+        System.out.print("Serie: ");
+
+        for (num=1;num<=100;num++)
+        {
+            System.out.print(num+",");
+        }
+    }
+    
+}
---
+++ ejercicio/src/ejercicio/Ejercicio.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicio;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author jvald
+ */
+public class Ejercicio {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+   
+     
+    int numeros [] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,100}
+         array.sort(numeros);
+         for (int i = numeros.length-1; i>=0 ; i==){
+         System.out.println(""+numeros[i]);
+         
+         }
+            }
+}
